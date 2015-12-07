af7678485cca81204fc56fb78f6d436f921f63a4
Joseph Pellegrino <jpellegrino@josephs-mbp.home>
2015-12-06 21:43:41 -0500

    Initial Commit

---
+++ Control Room/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Control Room
+//
+//  Created by Joseph Pellegrino on 12/6/15.
+//  Copyright © 2015 Joseph Pellegrino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
